8a129465124abafbe9b5f8973b8c4a42ac335a88
Parthiv Reddy <reddy89@purdue.edu>
2024-01-20 16:10:34 -0500

    Module done

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -22,4 +22,38 @@ module memory_control (
   // number of cpus for cc
   parameter CPUS = 1;
 
+
+
+
+  always_comb begin : REQUEST
+    ccif.iwait = 1;
+    ccif.dwait = 0;
+    ccif.iload = 0;
+    ccif.dload = ccif.ramload;
+    ccif.ramaddr = ccif.daddr;
+    ccif.ramstore = ccif.dstore;
+    ccif.ramWEN = 0;
+    ccif.ramREN = 0;
+
+    if(ccif.dREN)
+      begin
+        ccif.dwait = (ramstate == ACCESS) ? 0 : 1;
+        ccif.ramREN = ccif.dREN;
+      end
+
+    else if(ccif.dWEN)
+      begin
+        ccif.ramWEN = ccif.dWEN;
+        ccif.dwait = (ramstate == ACCESS) ? 0 : 1;
+      end
+
+    else if(ccif.iREN)
+      begin
+        ccif.iwait = (ramstate == ACCESS) ? 0 : 1;
+        ccif.iload = ccif.ramload;
+        ccif.ramaddr = ccif.iaddr;
+        ccif.ramREN = ccif.iREN;
+      end
+  end
+
 endmodule
