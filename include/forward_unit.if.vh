aeb2c7b5047996bd598510c4d321d7baec5ba2d8
Parthiv Reddy <reddy89@purdue.edu>
2024-02-12 13:09:05 -0500

    forward unit

---
+++ include/forward_unit.if.vh
@@ -0,0 +1,25 @@
+`ifndef FORWARD_UNIT_IF_VH
+`define FORWARD_UNIT_IF_VH
+
+// ram memory types
+`include "cpu_types_pkg.vh"
+
+interface forward_unit_if;
+
+  // import types
+  import cpu_types_pkg::*;
+
+  //conditions for the hazard
+
+  logic stage2_rs, stage2_rt, stage3_rd, stage4_rd;
+  logic stage3_RegWr, stage4_RegWr;
+  //
+  logic [1:0] forwardA, forwardB; //for stage 2
+
+  modport fu {
+    input stage2_rs, stage2_rt, stage3_rd, stage4_rd, stage3_RegWr, stage4_RegWr;
+    output forwardA, forwardB;
+  };
+endinterface
+`endif
+  
---
+++ source/forward_unit.sv
@@ -0,0 +1,42 @@
+`include "forward_unit_if.vh"
+`include "cpu_types_pkg.vh"
+
+module forward_unit(
+    forward_unit_if.fu fuif
+);
+import cpu_types_pkg::*;
+
+    always_comb begin 
+        fuif.forwardA = 2'b00;
+        fuif.forwardB = 2'b00;
+        if(fuif.stage3_RegWr && fuif.stage3_rd != 0)
+        begin
+
+            if(fuif.stage3_rd == fuif.stage2_rs)
+            begin
+                fif.forwardA = 2'b10;
+            end
+            if(fuif.stage3_rd == stage2_rt)
+            begin
+                fuif.forwardB = 2'b10;
+            end
+        end
+        if(fuif.stage4_RegWr && fuif.stage4_rd != 0)
+        begin
+            if(!(fuif.stage3_RegWr && (fuif.stage3_rd != 0) && (fuif.stage3_rd != fuif.stage2_rs)))
+            begin
+                if(fuif.stage4_rd == fuif.stage2_rs)
+                begin
+                    fuif.forwardA = 2'b01;
+                end
+            end
+            if(!(fuif.stage3_RegWr && (fuif.stage3_rd != 0) && (fuif.stage3_rd != fuif.stage2_rt)))
+            begin
+                if(fuif.stage4_rd == fuif.stage2_rt)
+                begin
+                    fuif.forwardB = 2'b01;
+                end
+            end
+        end
+    end
+endmodule
