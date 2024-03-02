d1cc6ad9b23adacec2a071acc2762560bdfcf281
Parthiv Reddy <reddy89@purdue.edu>
2024-03-02 14:35:38 -0500

    icache

---
+++ source/icache.sv
@@ -0,0 +1,92 @@
+`include "cpu_types_pkg.vh"
+`include "cache_control_if
+.vh"
+
+module icache(
+    datapath_cache_if.icache dcif,
+    cache_if.icache cif
+);
+
+
+
+import cpu_types_pkg::*;
+
+icache_frame [15:0] cache;
+icache_frame [15:0] ncache;
+
+icachef_t addr;
+
+assign addr.bytoff = dcif.imemaddr[1:0];
+assign addr.idx = dcif.imemaddr[5:2];
+assign addr.tag = dcif.imemaddr[31:6];
+
+typedef enum logic {IDLE, MISS} state;
+
+state currState;
+state nState;
+
+always_ff @(posedge CLK, negedge nRST) begin : nST
+    if(!nRST)
+    begin
+        currState <= IDLE;
+        cache <= '0;
+    end
+    else
+    begin
+        currState <= nState;
+        cache <= ncache;
+    end
+end
+
+always_comb begin : CMBLGC
+    nState = currState;
+    cif.iREN = 0;
+    cif.iaddr = 0;
+    dcif.ihit = 0;
+    dcif.imemload = 0;
+    if(imemREN)
+    begin
+        case(currState)
+            IDLE:
+            begin
+                if(cache[addr.idx].valid && cache[addr.idx].tag == addr.tag) //hit
+                begin
+                    dcif.ihit = 1;
+                    dcif.imemload = cache.data;
+                end
+                else if(!cache[addr.idx].valid || cache[addr.idx].tag != addr.tag) //miss
+                begin
+                    cif.iREN = 1;
+                    cif.iaddr = dcif.imemaddr;
+                    dcif.ihit = 0;
+                    nState = MISS;
+                end
+
+            end
+            MISS:
+            begin
+                if(cif.iwait) //possibly latch signals from IDLE->MISS
+                begin
+                    cif.iREN = 1;
+                    cif.iaddr = dcif.imemaddr;
+                    dcif.ihit = 0;
+                end
+                else
+                begin
+                    dcif.imemload = cif.iload;
+                    dcif.ihit = 1;
+                    ncache[addr.idx].tag = addr.tag
+                    ncache[addr.idx].data = cif.iload;
+                    ncache[addr.idx].valid = 1;
+                    nState = IDLE;
+                end
+            end
+        endcase
+
+    end
+
+    
+end
+
+
+endmodule
